18a2c16d0c6749e869f97765d033656968b45708
matsumo0922 <caios.system@gmail.com>
2025-08-25 12:09:14 +0900

    feat: Update app version and interstitial ad display logic
    - Update app version to 2.2.10 (versionCode 68) in `gradle/libs.versions.toml`.
    - Change the condition for `shouldShowInterstitialAd` in `core/model/src/commonMain/kotlin/me/matsumo/fanbox/core/model/Setting.kt` from a fixed 300 seconds to 1 day after the first launch.

--- core/model/src/commonMain/kotlin/me/matsumo/fanbox/core/model/Setting.kt
+++ core/model/src/commonMain/kotlin/me/matsumo/fanbox/core/model/Setting.kt
@@ -3,6 +3,7 @@ package me.matsumo.fanbox.core.model
 import androidx.compose.ui.text.intl.Locale
 import kotlinx.serialization.Serializable
 import kotlin.time.Clock
+import kotlin.time.Duration.Companion.days
 import kotlin.time.ExperimentalTime
 
 @Serializable
@@ -36,7 +37,7 @@ data class Setting(
     val isAllowedShowAdultContents get() = !isTestUser && isOverrideAdultContents
 
     @OptIn(ExperimentalTime::class)
-    val shouldShowInterstitialAd get() = (Clock.System.now().epochSeconds - firstLaunchTime) > 300L // 1日(=24*60*60秒)
+    val shouldShowInterstitialAd get() = (Clock.System.now().epochSeconds - firstLaunchTime) > 1.days.inWholeSeconds
 
     companion object Companion {
         @OptIn(ExperimentalTime::class)
